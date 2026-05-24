50edce40c1886d6ad77997469c62510e56f03f93
alrickdemetrius <alrick.demetrius@gmail.com>
2026-05-25 00:41:56 +0700

    cors update

--- config/cors.php
+++ config/cors.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:8100");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 
